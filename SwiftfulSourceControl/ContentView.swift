451bcf7605981cbc8c5a18ee4b932997aa7805a5
Amir <amir.lajevardi@city.ac.uk>
2024-10-26 10:33:42 +0100

    another commit

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -16,9 +16,6 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Swiftful thinking!")
             
-            Button("click me") {
-                
-            }
         }
         .padding()
     }
