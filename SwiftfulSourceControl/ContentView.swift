c08a9ec05a9aad3b6feb292363be8c54a29624e3
Amir <amir.lajevardi@city.ac.uk>
2024-10-26 15:11:03 +0100

    question mark is added

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -25,7 +25,7 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Swiftful Thinking!")
+            Text("Swiftful Thinking!?")
             Button("subscribe"){
             }
             Button("subscribe"){
